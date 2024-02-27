e31be652005db781b3104b9d503d462d0fbc3407
r3dpoint <3207964+r3dpoint@users.noreply.github.com>
2024-02-27 14:32:52 +0000

    Install Vanilla Extract https://vanilla-extract.style/documentation/integrations/remix/

--- remix-01/vite.config.ts
+++ remix-01/vite.config.ts
@@ -1,7 +1,8 @@
 import { vitePlugin as remix } from '@remix-run/dev';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [remix(), tsconfigPaths(), vanillaExtractPlugin()],
 });
